9630e13daa324f1b045595f0a4777dad2953220a
hubin999421 <hubin999421@03172679-f37d-c98b-3ea1-76b2e6b28464>
2011-08-06 16:59:17 +0000

    readme

--- trunk/src/tools/release/make.bat
+++ trunk/src/tools/release/make.bat
@@ -9,6 +9,7 @@ ECHO 正在从 %SVN_URI% 导出最新版本文件
 
 svn export --force %SVN_URI%lib lib
 svn export --force %SVN_URI%demo demo
+svn export --force %SVN_URI%readme.txt readme.txt
 
 copy appkey.php "demo/tencentappkey.php"
 copy appkey.php "demo/sinaappkey.php"
@@ -19,13 +20,14 @@ copy appkey.php "demo/163appkey.php"
 ECHO 文件导出成功...
 
 ECHO 开始打包
-%ZIP_URL% a OpenSDK.zip lib demo
+%ZIP_URL% a OpenSDK.zip lib demo readme.txt
 ECHO 创建压缩包完成
 
 ECHO 删除临时文件
 ping -n 5 127.1>nul
 rmdir lib /s/q
 rmdir demo /s/q
+del readme.txt
 
 ECHO 打包完成！
 
