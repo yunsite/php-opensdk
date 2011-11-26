ff46fb58a2803f8bfa48fb95e821d5143a9a13d0
hubin999421 <hubin999421@03172679-f37d-c98b-3ea1-76b2e6b28464>
2011-11-26 15:45:32 +0000

    demo use getParam & setParam

--- trunk/src/demo/163.php
+++ trunk/src/demo/163.php
@@ -19,18 +19,21 @@ header('Content-Type: text/html; charset=utf-8');
 $exit = false;
 if(isset($_GET['exit']))
 {
-	unset($_SESSION[OpenSDK_163_Weibo::OAUTH_TOKEN]);
-	unset($_SESSION[OpenSDK_163_Weibo::ACCESS_TOKEN]);
-	unset($_SESSION[OpenSDK_163_Weibo::OAUTH_TOKEN_SECRET]);
+    OpenSDK_163_Weibo::setParam(OpenSDK_163_Weibo::OAUTH_TOKEN, null);
+    OpenSDK_163_Weibo::setParam(OpenSDK_163_Weibo::ACCESS_TOKEN, null);
+    OpenSDK_163_Weibo::setParam(OpenSDK_163_Weibo::OAUTH_TOKEN_SECRET, null);
 	echo '<a href="?go_oauth">点击去授权</a>';
 }
-else if(isset($_SESSION[OpenSDK_163_Weibo::ACCESS_TOKEN]) && isset($_SESSION[OpenSDK_163_Weibo::OAUTH_TOKEN_SECRET]))
+else if(
+        OpenSDK_163_Weibo::getParam (OpenSDK_163_Weibo::ACCESS_TOKEN) &&
+        OpenSDK_163_Weibo::getParam (OpenSDK_163_Weibo::OAUTH_TOKEN_SECRET)
+        )
 {
 	//已经取得授权
 	$uinfo = OpenSDK_163_Weibo::call('users/show');
 	echo '你已经获得授权。你的授权信息:<br />';
-	echo 'Access token: ' , $_SESSION[OpenSDK_163_Weibo::ACCESS_TOKEN] , '<br />';
-	echo 'oauth_token_secret: ' , $_SESSION[OpenSDK_163_Weibo::OAUTH_TOKEN_SECRET] , '<br />';
+	echo 'Access token: ' , OpenSDK_163_Weibo::getParam (OpenSDK_163_Weibo::ACCESS_TOKEN) , '<br />';
+	echo 'oauth_token_secret: ' , OpenSDK_163_Weibo::getParam (OpenSDK_163_Weibo::OAUTH_TOKEN_SECRET) , '<br />';
 	echo '你的微博帐号信息为:<br /><pre>';
 	var_dump($uinfo);
 	$exit = true;
@@ -42,14 +45,13 @@ else if( isset($_GET['oauth_token']) )
 	{
 		$uinfo = OpenSDK_163_Weibo::call('users/show');
 		echo '从Opent返回并获得授权。你的微博帐号信息为：<br />';
-		echo 'Access token: ' , $_SESSION[OpenSDK_163_Weibo::ACCESS_TOKEN] , '<br />';
-		echo 'oauth_token_secret: ' , $_SESSION[OpenSDK_163_Weibo::OAUTH_TOKEN_SECRET] , '<br />';
+		echo 'Access token: ' , OpenSDK_163_Weibo::getParam (OpenSDK_163_Weibo::ACCESS_TOKEN) , '<br />';
+		echo 'oauth_token_secret: ' , OpenSDK_163_Weibo::getParam (OpenSDK_163_Weibo::OAUTH_TOKEN_SECRET) , '<br />';
 		echo '你的微博帐号信息为:<br /><pre>';
 		var_dump($uinfo);
 	}
 	else
 	{
-		var_dump($_SESSION);
 		echo '获得Access Tokn 失败';
 	}
 	$exit = true;
